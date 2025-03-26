ecec097874acd32cd842c18e752e6d17dc0cc758
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-03-26 18:14:20 +0300

    upgrade methods

--- src/main/java/ru/berezhnov/dao/BookDAO.java
+++ src/main/java/ru/berezhnov/dao/BookDAO.java
@@ -1,22 +1,21 @@
 package ru.berezhnov.dao;
 
 import ru.berezhnov.models.Book;
+import ru.berezhnov.models.Person;
 import ru.berezhnov.util.DBUtil;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class BookDAO {
 
     public List<Book> index() {
         List<Book> books = new ArrayList<>();
-        String sql = "SELECT * FROM book ORDER BY id";
-
         try (Connection connection = DBUtil.getConnection();
              Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
-
+             ResultSet rs = stmt.executeQuery("SELECT * FROM book ORDER BY id")) {
             while (rs.next()) {
                 books.add(mapBook(rs));
             }
@@ -27,26 +26,24 @@ public class BookDAO {
     }
 
     public Book show(int id) {
-        String sql = "SELECT * FROM book WHERE id = ?";
+        Book book = null;
         try (Connection connection = DBUtil.getConnection();
-             PreparedStatement stmt = connection.prepareStatement(sql)) {
-
+             PreparedStatement stmt = connection.prepareStatement("SELECT * FROM book WHERE id = ?")) {
             stmt.setInt(1, id);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
-                return mapBook(rs);
+                book = mapBook(rs);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return book;
     }
 
     public void save(Book book) {
         String sql = "INSERT INTO book (title, author, year, person_id) VALUES (?, ?, ?, NULL)";
         try (Connection connection = DBUtil.getConnection();
              PreparedStatement stmt = connection.prepareStatement(sql)) {
-
             stmt.setString(1, book.getTitle());
             stmt.setString(2, book.getAuthor());
             stmt.setInt(3, book.getYear());
@@ -60,7 +57,6 @@ public class BookDAO {
         String sql = "UPDATE book SET title = ?, author = ?, year = ? WHERE id = ?";
         try (Connection connection = DBUtil.getConnection();
              PreparedStatement stmt = connection.prepareStatement(sql)) {
-
             stmt.setString(1, book.getTitle());
             stmt.setString(2, book.getAuthor());
             stmt.setInt(3, book.getYear());
@@ -75,7 +71,6 @@ public class BookDAO {
         String sql = "DELETE FROM book WHERE id = ?";
         try (Connection connection = DBUtil.getConnection();
              PreparedStatement stmt = connection.prepareStatement(sql)) {
-
             stmt.setInt(1, id);
             stmt.executeUpdate();
         } catch (SQLException e) {
@@ -85,11 +80,11 @@ public class BookDAO {
 
     private Book mapBook(ResultSet rs) throws SQLException {
         return new Book(
-                rs.getInt("id"),
-                rs.getString("title"),
-                rs.getString("author"),
-                rs.getInt("year"),
-                (Integer) rs.getObject("person_id") // может быть NULL
+            rs.getInt("id"),
+            rs.getString("title"),
+            rs.getString("author"),
+            rs.getInt("year"),
+            (Integer) rs.getObject("person_id") // может быть NULL
         );
     }
 }
